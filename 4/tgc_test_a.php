a3398ab04ac51329d5a779b39db2531d202ebce1
Brian-4720 <brian@jonnes.co.za>
2021-03-09 13:08:02 +0200

    Tests: modules, files, and resources.

--- 4/tgc_test_a.php
+++ 4/tgc_test_a.php
@@ -27,6 +27,13 @@ class mtgc_module_test__modules {
 	}
 };
 
+class mtgc_module_test__urls {
+	public $a;
+	function __construct() {
+		$this->a = array();
+	}
+};
+
 class tgc_module_test__empty {
 	public $NF;
 	public $inner, $w;
@@ -64,12 +71,13 @@ class tgc_module_test__a {
 	public $NF;
 	public $path, $env;
 	public $testpath;
-	public $modules;
-	function __construct($path,$env,$testpath,$modules) {
+	public $modules, $urls;
+	function __construct($path,$env,$testpath,$modules,$urls) {
 		$this->path = $path;
 		$this->env = $env;
 		$this->testpath = $testpath;
 		$this->modules = $modules;
+		$this->urls = $urls;
 	}
 	function start( $q ) {
 		return 0; }
@@ -79,10 +87,26 @@ class tgc_module_test__a {
 		$q->write(str_replace("<","&lt;", str_replace( "&", "&amp;", $x ) ) ); }
 	function consume( $q, $end, $w ) {
 		if( $w->nodeName == 'module' ) {
+			if( $end ) return 1;
 			if( ! array_key_exists( $w->getAttribute('path'), $this->modules->a ) ) {
 				$this->modules->a[$w->getAttribute('path')] = 1;
 				$a = include $this->testpath . '/' . $w->getAttribute('path');
 			}
+			return 2; }
+		if( $w->nodeName == 'a' ) {
+			if( $end ) return 1;
+			if( ! array_key_exists( 'a ' . $w->getAttribute('href'), $this->urls->a ) ) {
+				$this->urls->a['a ' . $w->getAttribute('href')] = 1; }
+			return 2; }
+		if( $w->nodeName == 'link' ) {
+			if( $end ) return 1;
+			if( ! array_key_exists( 'link ' . $w->getAttribute('href'), $this->urls->a ) ) {
+				$this->urls->a['link ' . $w->getAttribute('href')] = 1; }
+			return 2; }
+		if( $w->nodeName == 'img' ) {
+			if( $end ) return 1;
+			if( ! array_key_exists( 'img ' . $w->getAttribute('src'), $this->urls->a ) ) {
+				$this->urls->a['img ' . $w->getAttribute('src')] = 1; }
 			return 2; }
 		if( $w->nodeName == 'include' ) {
 			if ($end) return 1;
@@ -143,6 +167,57 @@ class tgc_module_test__modules {
 		$a = 00; $d = $k = $n = null;
 		if( $w->nodeName == 'item' ) {
 			$this->NF = newframe(new tgc_module_test__modules_item($this->path,$this->env,$this->modules),$q,$w);
+			return 3;
+		}
+		return 0;
+	}
+};
+
+class tgc_module_test__url_item {
+	public $NF;
+	public $path, $env;
+	public $urls;
+	function __construct($path,$env,$urls) {
+		$this->path = $path;
+		$this->env = $env;
+		$this->urls = array_keys($urls->a);
+	}
+	function start( $q ) {
+		$this->m = 0;
+		return 0; }
+	function repeat( $q ) {
+		$this->m += 1;
+		return $this->m < count($this->urls); }
+	function consume_text( $q, $x ) {
+		$q->write(str_replace("<","&lt;", str_replace( "&", "&amp;", $x ) ) ); }
+	function consume( $q, $end, $w ) {
+		if( $w->nodeName == 'url_value' ) {
+			if( $end ) return 1;
+			$q->write( sr_amp_lt( $this->urls[$this->m] ) );
+			return 1; }
+		return 0;
+	}
+};
+
+class tgc_module_test__urls {
+	public $NF;
+	public $path, $env;
+	public $urls;
+	function __construct($path,$env,$urls) {
+		$this->path = $path;
+		$this->env = $env;
+		$this->urls = $urls;
+	}
+	function start( $q ) {
+		return 0; }
+	function repeat( $q ) {
+		return 0; }
+	function consume_text( $q, $x ) {
+		$q->write(str_replace("<","&lt;", str_replace( "&", "&amp;", $x ) ) ); }
+	function consume( $q, $end, $w ) {
+		$a = 00; $d = $k = $n = null;
+		if( $w->nodeName == 'item' ) {
+			$this->NF = newframe(new tgc_module_test__url_item($this->path,$this->env,$this->urls),$q,$w);
 			return 3;
 		}
 		return 0;
@@ -218,7 +293,8 @@ class mtgc_module_test {
 		$this->path = $path;
 		$this->env = $env;
 		$this->modules = new mtgc_module_test__modules;
-		$this->c = new tgc_module_test__a($path,$env,$_SERVER['DOCUMENT_ROOT'],$this->modules);
+		$this->urls = new mtgc_module_test__urls;
+		$this->c = new tgc_module_test__a($path,$env,$_SERVER['DOCUMENT_ROOT'],$this->modules,$this->urls);
 	}
 	function start( $q ) {
 		return 0; }
@@ -232,14 +308,18 @@ class mtgc_module_test {
 			if( $end ) return 1;
 			$n = opendir( $_SERVER['DOCUMENT_ROOT'] );
 			while( ( $a = readdir($n) ) !== false ) {
-				if( strtoupper( substr($a,strlen($a)-4) ) == strtoupper( $this->env->file_ext ) ) {
+				if( strtoupper( substr($a,strlen($a)-strlen($this->env->file_ext)) ) == strtoupper( $this->env->file_ext ) ) {
 					$d = load_eggsgml_file( $_SERVER['DOCUMENT_ROOT'] . '/' . $a );
 					$k = newframe($this->c, new null_buffer2, $d);
 					$k->P = null;
 					eggsgml($k);
 				}
-				$this->NF = newframe(new tgc_module_test__modules($this->path,$this->env,$this->modules),$q,$w);
 			}
+			$this->NF = newframe(new tgc_module_test__modules($this->path,$this->env,$this->modules),$q,$w);
+			return 3; }
+		if( $w->nodeName == 'url_list' ) {
+			if( $end ) return 1;
+			$this->NF = newframe( new tgc_module_test__urls($this->path,$this->env,$this->urls),$q,$w);
 			return 3; }
 		if( $w->nodeName == 'file_list' ) {
 			if( $end ) return 1;
